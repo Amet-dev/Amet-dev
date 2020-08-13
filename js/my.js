edac5f144c8de25ae6061d94acf0e2a043c13855
Amet-Han <velilyaev1999@bk.ru>
2020-08-13 22:44:48 +0300

    button

--- js/my.js
+++ js/my.js
@@ -1,7 +1,5 @@
-var typedText = document.getElementsByClassName('typed');
-
-
 var typed = new Typed('.typed', {
     stringsElement: '#typed-strings'
-    , typeSpeed: 150, backSpeed: 100, shuffle: true, loop: true, loopCount: Infinity, 
+    , typeSpeed: 150, backSpeed: 40, shuffle: true, loop: true, loopCount: Infinity, 
   });
+var typedText = document.getElementsByClassName('typed');
