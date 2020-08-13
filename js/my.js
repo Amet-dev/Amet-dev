6c081a31bb6d1f28aa5be84a6025881611c24ba5
Amet-Han <velilyaev1999@bk.ru>
2020-08-13 12:43:38 +0300

    add typedjs

---
+++ js/my.js
@@ -0,0 +1,7 @@
+var typedText = document.getElementsByClassName('typed');
+
+
+var typed = new Typed('.typed', {
+    stringsElement: '#typed-strings'
+    , typeSpeed: 150, backSpeed: 100, shuffle: true, loop: true, loopCount: Infinity, 
+  });
