410fede30bad02bdce1c940d8df9c935f31e275c
Amet-Han <velilyaev1999@bk.ru>
2020-08-14 00:04:08 +0300

    parallax and fix

--- js/my.js
+++ js/my.js
@@ -2,4 +2,13 @@ var typed = new Typed('.typed', {
     stringsElement: '#typed-strings'
     , typeSpeed: 150, backSpeed: 40, shuffle: true, loop: true, loopCount: Infinity, 
   });
-var typedText = document.getElementsByClassName('typed');
+console.log(navigator.userAgent);
+
+if (!( (screen.width<480) || (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent)) )){
+	let bg = document.querySelector('.mouse-parallax-bg');
+	window.addEventListener('mousemove', function(e) {
+	    let x = e.clientX / window.innerWidth;
+	    let y = e.clientY / window.innerHeight;  
+	    bg.style.transform = 'translate(-' + x * 50 + 'px, -' + y * 50 + 'px)';
+	});
+}
